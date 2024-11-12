e3da33967ce946f847a2f492612638924bc30952
Rainscario <r.a.v2018@mail.ru>
2024-11-13 00:25:52 +0300

    09-5

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -118,9 +118,22 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function (...args) {
+    let error;
+
+    for (let i = 0; i < attempts; i + 1) {
+      try {
+        return func.apply(this, args);
+      } catch (e) {
+        error = e;
+      }
+    }
+
+    throw error;
+  };
 }
+
 
 /**
  * Returns the logging wrapper for the specified method,
